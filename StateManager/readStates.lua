1d502a5ccdeb2e9be94b6f248414db8c7e4a8e89
Anna Leticia Alegria <annaalegria23@hotmail.com>
2021-09-28 14:10:33 -0300

    Converting tree into graph correctly

--- StateManager/readStates.lua
+++ StateManager/readStates.lua
@@ -65,6 +65,16 @@ function readStates(path)
   return 1
 end
 
+local function existsInTable (searchedTable, element)
+  for _, value in ipairs(searchedTable) do
+    if (value == element) then
+      return 1
+    end
+  end
+
+  return 0
+end
+
 local function insertEdge (entryNode, eventName, exitNode, eventRule)
   if (not graph[entryNode]) then
     graph[entryNode] = {}
@@ -72,7 +82,9 @@ local function insertEdge (entryNode, eventName, exitNode, eventRule)
   if (not graph[entryNode][eventName]) then
     graph[entryNode][eventName] = {{exitNode}, eventRule}
   else
-    table.insert(graph[entryNode][eventName][1], exitNode)
+    if (~existsInTable(graph[entryNode][eventName][1], exitNode)) then
+      table.insert(graph[entryNode][eventName][1], exitNode)
+    end
   end
 end
 
@@ -96,6 +108,8 @@ local function processTree (currentGraphNode, grammarTree)
         for _, value in ipairs(child1) do
           insertEdge (value[3], value[1], currentGraphNode, value[2])
         end
+
+        return child1
       else
         if(grammarTree["tag"] == "seq") then
           child1 = processTree (currentGraphNode, grammarTree[1])
@@ -120,7 +134,7 @@ function createGraph (grammarTree)
 
   if (lastChild) then
     for _,value in ipairs(lastChild) do 
-      insertEdge (value[3], value[1], globalGraphNode, value[2])
+      insertEdge (value[3], value[1], -1, value[2])
     end
   end
 
@@ -183,26 +197,27 @@ end
 
 readStates("../GraphTests/RW_statesFile2.txt")
 
-print("Chamada 1 -> "..tostring(checkEvent("WriterWantsToStart", 123)))
-print("Chamada 2 -> "..tostring(checkEvent("WriterStarts", 123)))
-print("Chamada 3 -> "..tostring(checkEvent("WriterWrites", 123)))
-print("Chamada 4 -> "..tostring(checkEvent("WriterEnds", 123)))
-print("Chamada 5 -> "..tostring(checkEvent("ReaderWantsToStart", 456)))
-print("Chamada 6 -> "..tostring(checkEvent("ReaderStarts", 456)))
-print("Chamada 7 -> "..tostring(checkEvent("ReaderReads", 456)))
-print("Chamada 8 -> "..tostring(checkEvent("ReaderEnds", 456)))
-print("Testando loops")
 
-for i = 1, 4 do
-  print("Chamada 1 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterWantsToStart", 123)))
-  print("Chamada 2 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterStarts", 123)))
-  print("Chamada 3 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterWrites", 123)))
-  print("Chamada 4 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterEnds", 123)))
-  print("Chamada 3 -> "..tostring(checkEvent("WriterWrites", 123)))
-end
-for i = 1, 4 do
-  print("Chamada 1 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderWantsToStart", 456)))
-  print("Chamada 2 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderStarts", 456)))
-  print("Chamada 3 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderReads", 456)))
-  print("Chamada 4 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderEnds", 456)))
-end
+-- print("Chamada 1 -> "..tostring(checkEvent("WriterWantsToStart", 123)))
+-- print("Chamada 2 -> "..tostring(checkEvent("WriterStarts", 123)))
+-- print("Chamada 3 -> "..tostring(checkEvent("WriterWrites", 123)))
+-- print("Chamada 4 -> "..tostring(checkEvent("WriterEnds", 123)))
+-- print("Chamada 5 -> "..tostring(checkEvent("ReaderWantsToStart", 456)))
+-- print("Chamada 6 -> "..tostring(checkEvent("ReaderStarts", 456)))
+-- print("Chamada 7 -> "..tostring(checkEvent("ReaderReads", 456)))
+-- print("Chamada 8 -> "..tostring(checkEvent("ReaderEnds", 456)))
+-- print("Testando loops")
+
+-- for i = 1, 4 do
+--   print("Chamada 1 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterWantsToStart", 123)))
+--   print("Chamada 2 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterStarts", 123)))
+--   print("Chamada 3 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterWrites", 123)))
+--   print("Chamada 4 loop i=" ..tostring(i).." -> "..tostring(checkEvent("WriterEnds", 123)))
+--   print("Chamada 3 -> "..tostring(checkEvent("WriterWrites", 123)))
+-- end
+-- for i = 1, 4 do
+--   print("Chamada 1 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderWantsToStart", 456)))
+--   print("Chamada 2 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderStarts", 456)))
+--   print("Chamada 3 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderReads", 456)))
+--   print("Chamada 4 loop i=" ..tostring(i).." -> "..tostring(checkEvent("ReaderEnds", 456)))
+-- end
